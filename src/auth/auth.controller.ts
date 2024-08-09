a76ee89a48bf3572dbe346ec12b6800efc8580d6
Samir Mahmud <samir93bj@gmail.com>
2024-08-09 16:23:44 -0300

    chore: Add message patterns for user registration, login, and token verification

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,18 @@
+import { MessagePattern } from '@nestjs/microservices';
+
 import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @MessagePattern({ cmd: 'auth.regiter.user' })
+  registerUser() {}
+
+  @MessagePattern({ cmd: 'auth.login.user' })
+  loginUser() {}
+
+  @MessagePattern({ cmd: 'auth.verify.user' })
+  verifyToken() {}
 }
