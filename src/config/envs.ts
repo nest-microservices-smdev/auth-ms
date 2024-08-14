fc50e9ca38ba3afdc491f7b907e4185e8660a835
samir93bj <samir93bj@gmail.com>
2024-08-14 13:51:28 -0300

    feat: Add User model and Prisma schema

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,11 +3,13 @@ import * as joi from 'joi';
 
 interface EnvVars {
   NATS_SERVERS: string[];
+  DATABASE_URL: string;
 }
 
 const envsSchema = joi
   .object({
     NATS_SERVERS: joi.array().items(joi.string()).required(),
+    DATABASE_URL: joi.string().required(),
   })
   .unknown(true);
 
@@ -22,4 +24,5 @@ const envVars: EnvVars = value;
 
 export const envs = {
   natsServers: envVars.NATS_SERVERS,
+  databaseUrl: envVars.DATABASE_URL,
 };
